5dc75f97b9d2d451cb02df122a9a76987bf2380f
ravo andria <ravoravo.ramily@gmail.com>
2023-09-30 13:44:10 -0700

    Remove dossier Macosx

--- Apollo/assets/js/functions.js
+++ Apollo/assets/js/functions.js
@@ -1,15 +1,26 @@
-$( document ).ready(function() {
-	
-function nav(){
-	
-	$('.nav-toggle').click(function(){
-		
-		$('.nav').toggleClass('open');
-		
+$(document).ready(function () {
+
+	function smoothScroll(target) {
+	  $('html, body').animate({
+		scrollTop: $(target).offset().top
+	  }, 800); 
+	}
+  
+	$('.nav-link').on('click', function (e) {
+	  e.preventDefault(); 
+  
+	  var target = $(this).attr('href'); 
+  
+	  smoothScroll(target);
+  
 	});
-	
-}
-
-nav();
-
-});
+  
+	function nav() {
+	  $('.nav-toggle').click(function () {
+		$('.nav').toggleClass('open');
+	  });
+	}
+  
+	nav();
+  });
+  
