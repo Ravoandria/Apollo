a8d16d61f3242f5a24c0755dd9ae5b6b3133892b
ravo andria <ravoravo.ramily@gmail.com>
2023-10-02 20:24:02 -0700

    Ajout Leaflet

--- Apollo/assets/js/functions.js
+++ Apollo/assets/js/functions.js
@@ -60,3 +60,10 @@ nextButton.addEventListener("click", () => {
 
 
 showSlide(currentSlide);
+
+var mymap = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
